620406722a6e3d346f7c843d63513c4d654bac7b
bberak <bberak@gmail.com>
2019-09-07 00:17:04 +1000

    Removed unused var

--- game/graphics/shaders/scanline-shader.js
+++ game/graphics/shaders/scanline-shader.js
@@ -13,11 +13,9 @@ export default (thickness = 5.0, color = new THREE.Vector4(0, 0, 0, 1)) => {
 		void main() {
 			vUv = uv;
 			gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-		}
-	`,
+		}`,
 
 		fragmentShader: `
-		const vec4 blank = vec4(0.0, 0.0, 0.0, 1.0);
 		uniform sampler2D tDiffuse;
 		uniform float thickness;
 		uniform vec4 color;
@@ -25,8 +23,7 @@ export default (thickness = 5.0, color = new THREE.Vector4(0, 0, 0, 1)) => {
 		void main() {
 			float result = floor(mod(gl_FragCoord.y, thickness));
 			gl_FragColor = result == 0.0 ? texture2D(tDiffuse, vUv) : color;
-		}
-	`
+		}`
 	};
 
 	return scanlineShader;
