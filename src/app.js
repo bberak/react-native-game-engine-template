c7db101b6ea6c1b3e1dfd47664687c7776c42b60
bberak <bberak@gmail.com>
2019-10-11 17:14:37 +1100

    Reorganized files to be in the src folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 import Game from "./game";
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <Game />
     );
   }
 }
+
+registerRootComponent(App);
